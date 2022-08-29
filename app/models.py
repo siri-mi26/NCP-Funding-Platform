2edb111c8339b195b753b4536f81e0c2c41f2f8e
22709093 <22709093@student.uwa.edu.au>
2022-08-29 15:08:34 +0800

    Limited login functionality & temp sqlite db to test login
    Working login with password hash, still needs salt. Temp sqlite db implemented for login. No registration func. Messy, needs clean-up and more security features.

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    """temp create login user table with sqlite.
+    can be removed when boys are done with db
+    unless we decide to keep"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(180))
+    #email should probs/could be included & auth level. But not must have
+    
+   
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,47 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect, request
 from app import app
+from app.forms import LoginForm
+from flask_login import current_user, login_user, login_required, logout_user
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
-
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
-    user = {"username": "Daisy"}
-    return render_template('index.html', title="Home", user=user)
+    """Routes for index page"""
+    return render_template('index.html', title="Home")
 
 @app.route('/queries')
+@login_required
 def queries():
+    """Routes for queries"""
     return render_template('queries.html', title="Queries")
 
 @app.route('/downloads')
+@login_required
 def downloads():
+    """Routes for downloads page"""
     return render_template('downloads.html', title="Downloads")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
