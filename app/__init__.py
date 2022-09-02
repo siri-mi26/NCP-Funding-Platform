7f3a760696dbc23703c7f96ed52f8b8be578c6df
22709093 <22709093@student.uwa.edu.au>
2022-09-02 17:46:27 +0800

    More Sqlite test db to test search, and flask admin view
    Still needs user authentication for flask admin. Can edit entries.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,4 +13,18 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+admin = Admin(app, name='NCP', template_mode='bootstrap3')
+
+from app import db, routes, models
+from app.models import User, Student, University, Grants
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Student, db.session))
+admin.add_view(ModelView(Grants, db.session))
+admin.add_view(ModelView(University, db.session))
+#admin.add_view(ModelView(Grants, db.session))
+# Add administrative views here
+
+#app.run()
--- app/models.py
+++ app/models.py
@@ -24,14 +24,31 @@ class User(UserMixin, db.Model):
 class Student(db.Model):  
     __tablename__ = 'Student' 
     id = db.Column(db.Integer, primary_key = True) 
-    studentnumber = db.Column(db.Integer)
-    firstname = db.Column(db.String(50))
-    lastname = db.Column(db.String(50))
-    university = db.Column(db.String(50)) #could be foreign key to diff table
+    student_Number = db.Column(db.Integer)
+    First_Name = db.Column(db.String(50))
+    Last_Name = db.Column(db.String(50))
+    University = db.Column(db.String(50)) #could be foreign key to diff table
     
     def __repr__(self):
         return '<Student {}>'.format(self.studentnumber)
-    
+
+class University(db.Model):  
+    __tablename__ = 'University' 
+    id = db.Column(db.Integer, primary_key = True) 
+    Name = db.Column(db.Integer)
+    Member_Status = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<University {}>'.format(self.Name)
+
+class Grants(db.Model):  
+    __tablename__ = 'Grants' 
+    id = db.Column(db.Integer, primary_key = True) 
+    Name = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Grant {}>'.format(self.Name)
+        
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
