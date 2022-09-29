e35fd4879ea9ec1f4580d1d080b9de84bb215112
22709093 <22709093@student.uwa.edu.au>
2022-09-29 17:24:39 +0800

    Merge branch 'sandbox' of https://github.com/WeidongChen1026/NCP_group_37 into sandbox

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 #add admin views for tables/models
 admin = Admin(app, name='NCP', template_mode='bootstrap3', index_view=MyAdminIndexView()) 
 admin.add_link(LogoutMenuLink(name='Logout', category='', url="/logout"))
-#admin.add_view(SecureModelView(Users, db.session))ss
+#admin.add_view(SecureModelView(Users, db.session))
 admin.add_view(InfoView(name='Information', endpoint='info'))
 admin.add_view(StudentsModelView(Students, db.session))
 admin.add_view(ProgramsModelView(Programs, db.session))
@@ -32,7 +32,6 @@ admin.add_view(PaymentsModelView(Payments, db.session))
 admin.add_view(UniversitiesModelView(Universities, db.session))
 admin.add_view(CampusesModelView(Campuses, db.session))
 admin.add_view(GrantsModelView(Grants, db.session))
-
 
 
 
