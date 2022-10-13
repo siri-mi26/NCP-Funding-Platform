38832c24b5a8c1b75f030cb42a9121746cc728cf
missi982 <missi982@student.otago.ac.nz>
2022-10-14 06:17:42 +0800

    Merged grants and payments

--- app/ncp.py
+++ app/ncp.py
@@ -1,6 +1,8 @@
 from app import app,db
-from app.models import  Users, Students, Programs, Payments, Universities, Campuses, Grants, ProgramsByUniversity, UniversitiesFunding
+# from app.models import  Users, Students, Programs, Payments, Universities, Campuses, Grants, ProgramsByUniversity, UniversitiesFunding
+from app.models import  Users, Students, Programs, Universities, Campuses, Grants, ProgramsByUniversity, UniversitiesFunding
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Users': Users, 'Students': Students, 'Programs': Programs, 'Payments': Payments, "Universities": Universities, "Campuses": Campuses, 'Grants':Grants, 'ProgramsByUniversity': ProgramsByUniversity, 'UniversitiesFunding': UniversitiesFunding}
+    # return {'db': db, 'Users': Users, 'Students': Students, 'Programs': Programs, 'Payments': Payments, "Universities": Universities, "Campuses": Campuses, 'Grants':Grants, 'ProgramsByUniversity': ProgramsByUniversity, 'UniversitiesFunding': UniversitiesFunding}
+    return {'db': db, 'Users': Users, 'Students': Students, 'Programs': Programs, "Universities": Universities, "Campuses": Campuses, 'Grants':Grants, 'ProgramsByUniversity': ProgramsByUniversity, 'UniversitiesFunding': UniversitiesFunding}
